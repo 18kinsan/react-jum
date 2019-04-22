7982ca9674e1928788640735ed33c11fb417a96d
18kinsan <fridayars@gmail.com>
2019-04-22 10:09:34 +0700

    props intermediete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Product from "./Pros";
+import Product from "./Product";
 import Kons from './kons.js';
 import Buttons from './ButtonFloats';
 import './materialize/css/materialize.min.css';
+
 
 // import '.materialize/css/materialize.css';
 // import '.materialize/js/materialize.js';
@@ -13,40 +15,48 @@ class App extends Component {
     super();
     this.state ={
       products : [
-        // {
-        //   judul : 'Dalaman',
-        //   harga : '1.000'
-        // },
-        // {
-        //   judul : 'Luaran',
-        //   harga : '10.000'
-        // },
-        // {
-        //   judul : 'Sampingan',
-        //   harga : '5.000'
-        // },
-        // {
-        //   judul : 'Bawahan',
-        //   harga : '15.000'
-        // },
+        {
+          judul : 'Dalaman',
+          harga : '1.000'
+        },
+        {
+          judul : 'Luaran',
+          harga : '10.000'
+        },
+        {
+          judul : 'Sampingan',
+          harga : '5.000'
+        },
+        {
+          judul : 'Bawahan',
+          harga : '15.000'
+        },
+        
         
       ]
     }
   }
 
-  componentDidMount(){
-    let api_url = 'http://my-json-server.typicode.com/techtona/product_api2/products';
-    fetch(api_url)
-    .then(response => response.json())
-    .then(data=> {
-      this.setState({products : data});
-    })
-    .catch(function(e){
-      console.log(e);
-    })
+  hapusCard = (key) => {
+    this.state.delete_clicked = !this.state.delete_clicked;
+    this.setState({delete_clicked : this.state.delete_clicked});
+    this.state.products.splice(key.target.id,1);
+    this.setState({products : this.state.products});
+  }
+
+  // componentDidMount(){
+  //   let api_url = 'http://my-json-server.typicode.com/techtona/product_api2/products';
+  //   fetch(api_url)
+  //   .then(response => response.json())
+  //   .then(data=> {
+  //     this.setState({products : data});
+  //   })
+  //   .catch(function(e){
+  //     console.log(e);
+  //   })
 
     
-  }
+  // }
 
   hapusBaris = (key) => {
     this.state.products.splice(key,1);
@@ -56,26 +66,38 @@ class App extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-
+    let id=this.refs.id.value;
     let judul=this.refs.judul.value;
     let harga=this.refs.harga.value;
-    let id=this.refs.id.value;
+    
 
-    console.log(judul, harga, id);
+    // console.log(judul, harga, id);
+    this.state.products.push({id,judul,harga});
+        this.setState({products: this.state.products});
+
+        this.refs.form.reset();
+
+  }
+
+  
+  componentDidUpdate(prevProps, prevState, snapshot) {
 
   }
 
   render() {
     return (
       <div className="row">
-        <div className="col m12">
-            <form >
-              
-            </form>
+        <div className="col m3">
+        <form ref="form" onSubmit={this.handleSubmit}>
+            <input type="text" id="id" placeholder="ID Product"></input>
+            <input type="text" id="nama" placeholder="Nama Product"></input>
+            <input type="text" id="harga" placeholder="Harga"></input>
+            <button type="submit" className="waves-effect waves-light btn">Submit</button>
+          </form>
         </div>
 
         
-        <table>
+        {/* <table>
           <thead>
             <tr>
               <th>Nama Produk</th>
@@ -96,7 +118,20 @@ class App extends Component {
           )
           })}
             </tbody>
-          </table>
+          </table> */}
+
+
+          <div className="col m9">
+          {this.state.products.map((data, key) => {
+          return (
+          <Product deleteCard={this.hapusCard} harga={data.harga} judul={data.judul} key={key} id={key}/>
+          )
+          })}
+          </div>
+                    
+ 
+        
+            
         
       </div>
     );
--- src/Product.js
+++ src/Product.js
@@ -1,8 +1,30 @@
 import React, {Component} from 'react';
+import logo from './logo.svg'
+import './App.css'
 
-class Product extends React.Component{
-    render(){
-        return (<div><h1>{this.props.judul}</h1><h3>Harga : {this.props.harga}</h3></div>);
+class Product extends Component {
+    constructor(){
+        super();
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div className="col m3">
+                <div className="card teal darken-2">
+                    <div className="card-image white-text">
+                        <img src={logo} className="img-responsive"/>
+                    </div>
+                    <div className="card-content white-text">
+                        <h3 className="card-title"><strong>{this.props.judul}</strong></h3>
+                        <h4>Rp. {this.props.harga}</h4>
+                    </div>
+                    <div className="card-action">
+                    <a class="btn-floating btn-large pulse #d50000 red accent-4" onClick={this.props.deleteCard} id={this.props.id} href="#"><i class="material-icons">delete</i></a>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
